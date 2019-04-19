786797efcf28b905a51072d7179d53f7da049297
tkhr-saito <tkhr.saito@gmail.com>
2019-04-19 14:44:35 +0900

    move object

---
+++ Assets/BallController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    float speed = 20.0f;
+    Rigidbody rgbody;
+	// Use this for initialization
+	void Start () {
+        rgbody = GetComponent<Rigidbody>();
+        rgbody.AddForce((transform.forward + transform.right) * speed, ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/RocketController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour {
+
+    float Accel = 1000.0f;
+    Rigidbody rgbody;
+
+	// Use this for initialization
+	void Start () {
+        rgbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rgbody.AddForce(transform.right * Input.GetAxisRaw("Horizontal") * Accel, ForceMode.Impulse);
+	}
+}
