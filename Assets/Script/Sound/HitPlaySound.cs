a042a6749f30d2478f0f231d94082374e475eaef
tkhr-saito <tkhr.saito@gmail.com>
2019-04-23 11:41:57 +0900

    add effects

---
+++ Assets/Script/Sound/HitPlaySound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour {
+
+    public AudioClip sound;
+    void OnCollisionEnter(Collision collision)
+    {
+        AudioSource.PlayClipAtPoint(sound, collision.transform.position);    
+    }
+}
